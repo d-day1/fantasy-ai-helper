01dab1093cccfa83131f7f4910cef6b240adc16c
d-day1 <mamusemavie@gmail.com>
2025-06-15 00:42:49 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+    <h1>Fantasy AI Helper</h1>
+    <p>이곳은 당신의 소설 아이디어를 정리하는 AI 도우미입니다.</p>
+  </div>;
+}
